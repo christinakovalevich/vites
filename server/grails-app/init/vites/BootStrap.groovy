b48f66e446af85b25997e0cfd143e46e9ad47689
odinrossy <remniov.work@gamil.com>
2020-04-16 20:20:57 +0300

    FEAT add new users during the app launch

--- server/grails-app/init/vites/BootStrap.groovy
+++ server/grails-app/init/vites/BootStrap.groovy
@@ -1,6 +1,10 @@
 package vites
 
+import enums.security.RoleTypes
 import grails.gorm.transactions.Transactional
+import security.Role
+import security.User
+import security.UserRole
 
 class BootStrap {
 
@@ -12,6 +16,12 @@ class BootStrap {
 
     @Transactional
     private initSecurity() {
-        def role
+        def adminRole = new Role(authority: RoleTypes.ROLE_ADMIN.value).save()
+        def adminUser = new User(username: 'admin', password: 'admin').save()
+        UserRole.create(adminUser, adminRole)
+
+        def managerRole = new Role(authority: RoleTypes.ROLE_MANAGER.value).save()
+        def managerUser = new User(username: 'manager', password: 'manager').save()
+        UserRole.create(managerUser, managerRole)
     }
 }
