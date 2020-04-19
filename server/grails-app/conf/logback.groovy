8f9eb9d765f6b25e1548b203eea060674116eb04
OdinRossy <remniov.work@gmail.com>
2020-04-19 23:00:01 +0300

    FEAT adjust logging configuration

--- server/grails-app/conf/logback.groovy
+++ server/grails-app/conf/logback.groovy
@@ -34,8 +34,13 @@ if (Environment.isDevelopmentMode() && targetDir != null) {
     }
     logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false)
 }
-logger('org.hibernate.type', WARN)
-logger('org.hibernate', WARN)
-logger('org.springframework', WARN)
+// logger("org.hibernate.SQL", DEBUG, ['SQL_APPENDER'], false) // log statements
+// logger("org.hibernate.type.descriptor.sql.BasicBinder", TRACE, ['SQL_APPENDER'], false) // log parameters
+
+logger('org.springframework', INFO)
+logger('org.hibernate', INFO)
+logger('org.grails', INFO)
+logger('grails', INFO)
+logger('org.quartz', INFO)
 
 root(DEBUG, ['STDOUT'])
