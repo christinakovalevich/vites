185a8a3ba15554fff4e5e0535e362249af359e51
OdinRossy <remniov.work@gmail.com>
2020-04-19 23:20:05 +0300

    FEAT adjust BootStrap

--- server/grails-app/init/vites/BootStrap.groovy
+++ server/grails-app/init/vites/BootStrap.groovy
@@ -1,6 +1,7 @@
 package vites
 
-import enums.security.RoleTypes
+import enums.security.RoleType
+import enums.security.UserType
 import grails.gorm.transactions.Transactional
 import security.Role
 import security.User
@@ -16,12 +17,30 @@ class BootStrap {
 
     @Transactional
     private initSecurity() {
-        def adminRole = new Role(authority: RoleTypes.ROLE_ADMIN.value).save()
-        def adminUser = new User(username: 'admin', password: 'admin').save()
+        log.debug 'Start init security'
+
+        def adminRole = new Role(authority: RoleType.ROLE_ADMIN.value).save(FAIL_ON_ERROR)
+
+        def adminUser = new User(
+                username: 'admin',
+                password: 'admin',
+                userType: UserType.ADMIN
+        ).save(FAIL_ON_ERROR)
+
         UserRole.create(adminUser, adminRole)
 
-        def managerRole = new Role(authority: RoleTypes.ROLE_MANAGER.value).save()
-        def managerUser = new User(username: 'manager', password: 'manager').save()
+        def managerRole = new Role(authority: RoleType.ROLE_MANAGER.value).save(FAIL_ON_ERROR)
+
+        def managerUser = new User(
+                username: 'manager',
+                password: 'manager',
+                userType: UserType.MANAGER
+        ).save(FAIL_ON_ERROR)
+
         UserRole.create(managerUser, managerRole)
+
+        log.debug 'Finish init security'
     }
+
+    private final static FAIL_ON_ERROR = [failOnError: true]
 }
