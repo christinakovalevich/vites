dd27398b358aedce42c1ed0468176b41fb4566a8
OdinRossy <remniov.work@gmail.com>
2020-05-04 19:49:08 +0300

    FIX: rename pjson to packageJson

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,6 +1,6 @@
-import pjson from '../../package.json';
+import packageJson from '../../package.json';
 
 export const SERVER_URL = 'http://localhost:8080/';
-export const CLIENT_VERSION = pjson.version;
-export const REACT_VERSION = pjson.dependencies.react;
-export const APP_NAME = pjson.app.name;
+export const CLIENT_VERSION = packageJson.version;
+export const REACT_VERSION = packageJson.dependencies.react;
+export const APP_NAME = packageJson.app.name;
